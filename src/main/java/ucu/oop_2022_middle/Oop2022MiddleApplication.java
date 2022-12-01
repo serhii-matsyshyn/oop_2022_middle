f59b88b6e50c9c40707bf3a4b68a6ecfb2b8f1b2
Serhii Matsyshyn <serhii.matsyshyn@ucu.edu.ua>
2022-12-01 22:52:56 +0200

    initial commit

---
+++ src/main/java/ucu/oop_2022_middle/Oop2022MiddleApplication.java
@@ -0,0 +1,13 @@
+package ucu.oop_2022_middle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oop2022MiddleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oop2022MiddleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ucu/oop_2022_middle/Oop2022MiddleApplicationTests.java
@@ -0,0 +1,13 @@
+package ucu.oop_2022_middle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oop2022MiddleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
